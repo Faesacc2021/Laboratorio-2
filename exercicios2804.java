477d0e2aa34ca5c189d82c8fa743262daf4deb9a
Alunos CC Faesa 02 2021 <103604848+Faesacc2021@users.noreply.github.com>
2022-04-27 19:57:31 -0300

    Add files via upload

---
+++ exercicios2804.java
@@ -0,0 +1,15 @@
+
+public class exercicios2804 {
+
+	public static void main(String[] args) {
+		//Soma valores
+		somaValores(15.5, 18.5);
+	}
+
+	static void somaValores(double valor01, double valor02){
+		double resultado = valor01 + valor02;
+		System.out.printf("A soma de dois valores é: %2f", resultado );
+	}
+	
+
+}
