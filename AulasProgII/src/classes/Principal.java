7780c3eee6ec516113aef4541c9845db728dc2e8
Alunos Laboratorio <aluno.laboratorio@faesa.br>
2022-05-12 20:57:08 -0300

    Aula Laboratorio

--- AulasProgII/src/classes/Principal.java
+++ AulasProgII/src/classes/Principal.java
@@ -8,12 +8,43 @@ public class Principal {
 	}
 	
 	public static void iniciar(){
+		retangulo();
+		circulo();
+	}
+
+	public static void retangulo(){
+		Retangulo retangulo = new Retangulo();
+		Ponto ponto = new Ponto();
+		ponto.setX(1);
+		ponto.setY(1);
+		double base = 15.5;
+		double altura = 25.80;
+		
+		double area = 0.0;
+		double perimetro = 0.0;
+		area = retangulo.areaRetangulo(ponto, base, altura);
+		perimetro = retangulo.perimetroRetangulo(ponto, base, altura);
+		
+		System.out.printf("A área do retângulo = %.2f\n",  area);
+		System.out.printf("O perímetro do retângulo = %.2f\n\n", perimetro);		
+	}
+
+	public static void circulo(){
 		Circulo circulo = new Circulo();
 		Ponto ponto = new Ponto();
-		ponto.setX(55.5);
-		ponto.setY(66.6);
-		circulo.setPonto(ponto);
-		circulo.setRaio(44.4);
-		System.out.println(circulo.toString());
+		ponto.setX(1);
+		ponto.setY(1);
+		double raio = 15.5;
+
+		double area = 0.0;
+		double perimetro = 0.0;
+		area = circulo.areaRetangulo(ponto, raio);
+		perimetro = circulo.perimetroRetangulo(ponto, raio);
+		
+		System.out.printf("A área do circulo = %.2f\n",  area);
+		System.out.printf("O perímetro do circulo = %.2f\n", perimetro);
+	}
+
+	public static void triangulo(){
 	}
 }
